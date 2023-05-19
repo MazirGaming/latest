fa41fe157623d33999bcad6289516a796b642257
Mazir <nguyenmanh10a7@gmail.com>
2023-05-19 09:37:39 +0700

    first commit

---
+++ config/app.php
@@ -0,0 +1,23 @@
+<?php
+ return array (
+  'session' => 
+  array (
+    'driver' => 'php',
+  ),
+  'email' => 
+  array (
+    'driver' => 'mail',
+  ),
+  'cache' => 
+  array (
+    'driver' => 'file',
+    'servers' => 
+    array (
+    ),
+    'options' => 
+    array (
+    ),
+  ),
+  'cronkey' => 'Vwz9Jrbd0LxVgwbRBItNynnriqXXYOIS',
+  'key' => 'A55h2ayGdJNg5MGTFplwp00FyQAK96ey',
+);
